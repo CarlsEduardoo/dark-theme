a93bdf13b3602abe318931141341f5efc322995d
CarlsEduardoo <Carlos.eduardodk60@gmail.com>
2023-04-28 12:24:39 -0400

    Projeto Angular Dark Mode

---
+++ src/app/core/components/header/enums/Etheme.enums.ts
@@ -0,0 +1,7 @@
+export enum Etheme{
+    ICON_MOON = 'moon',
+    ICON_SUN = 'sun',
+
+    TEXT_SUN = 'light theme',
+    TEXT_MOON = 'dark theme',
+}
---
+++ src/app/core/components/header/header.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Etheme } from './enums/Etheme.enums';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  public icon: string = Etheme.ICON_MOON;
+
+  public textTheme: string = Etheme.TEXT_MOON;
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  public toggle() {
+    const theme = document.body.classList.toggle('dark-theme');
+
+    if (theme) {
+      this.textTheme = Etheme.TEXT_SUN;
+      return this.icon = 'sun';
+    }
+    this.textTheme = Etheme.TEXT_MOON;
+    return (this.icon = 'moon')
+  }
+
+}
